0151b28b8387bd68abffc67aadf2f60d0e30d200
Darwin D. Wu <darwin67@users.noreply.github.com>
2026-09-09 18:15:07 -0700

    feat(flutter): integrate Rust bridge on iOS

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -91,6 +91,11 @@ ExternalLibrary? nativeLibrary() {
       '$executableDirectory/../Frameworks/libshosai_flutter_bridge.dylib',
     );
   }
+  if (Platform.isIOS) {
+    return ExternalLibrary.open(
+      'shosai_flutter_bridge.framework/shosai_flutter_bridge',
+    );
+  }
   return null;
 }
 
